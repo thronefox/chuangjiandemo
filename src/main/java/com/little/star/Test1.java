161e7dc714d0e7f000f2822ea4f145599d112844
thronefox <36251447+thronefox@users.noreply.github.com>
2020-04-12 19:11:23 +0800

    origion modified!
    origion modified!

--- src/main/java/com/little/star/Test1.java
+++ src/main/java/com/little/star/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello world 本地主干修改");
         System.out.println("分支进行修改");
+        System.out.println("远程有人修改代码");
     }
 }
