08287b58fd42ad8b3aa230e661f2b482bca6b2fb
chenxing <thronefox@163.com>
2020-04-12 19:21:22 +0800

    合并代码

--- src/main/java/com/little/star/Test1.java
+++ src/main/java/com/little/star/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
         System.out.println("hello world 本地主干修改");
         System.out.println("分支进行修改");
         System.out.println("远程有人修改代码");
+        System.out.println("conflict demo");
         System.out.println("冲突演示");
     }
 }
